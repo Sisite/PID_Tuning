34a2ae07116f84463e64ca048cc87982289a6b31
Robin <jonrob-3@student.ltu.se>
2017-04-22 17:53:42 +0200

    Add project files.

---
+++ PIDTuner1/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace PIDTuner1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort serialPort;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            getComPorts();
+
+            
+        }
+
+        private void getComPorts ()
+        {
+            string[] comPorts = SerialPort.GetPortNames();
+            ComboBoxItem tmpItem;
+            foreach (string port in comPorts)
+            {
+                tmpItem = new ComboBoxItem();
+                tmpItem.Tag = port;
+                tmpItem.Content = port;
+                comPortList.Items.Add(tmpItem);
+
+            }
+            if (comPortList.Items.Count > 0)
+            {
+                (comPortList.Items[0] as ComboBoxItem).IsSelected = true;
+            }
+        }
+
+
+
+
+        void initConnection(Int32 portNR)
+        {
+
+        }
+
+        private void connectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int sBit = 1; 
+            if (comPortList.SelectedItem != null && baudRateList.SelectedItem != null)
+            {
+                string portName = (comPortList.SelectedItem as ComboBoxItem).Tag.ToString();
+                int baudRate = Int32.Parse((string)(baudRateList.SelectedItem as ComboBoxItem).Tag);
+                if (serialPort != null)
+                {
+                    serialPort.Close();
+                    serialPort = null; 
+                }
+                serialPort = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
+
+            }
+
+        }
+
+        private void tuneBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
